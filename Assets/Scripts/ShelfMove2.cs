420ef0cf94c814c72fd40f7da1618f0cab2df32e
anko0116 <hyukahn@umich.edu>
2021-09-02 18:15:10 -0400

    Implement bookshelf drag movement

--- Assets/Scripts/ShelfMove2.cs
+++ Assets/Scripts/ShelfMove2.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class ShelfMove : MonoBehaviour
+public class ShelfMove2 : MonoBehaviour
 {
     bool grabbed;
     GameObject grabbedObj;
@@ -18,24 +18,38 @@ public class ShelfMove : MonoBehaviour
     }
 
     void Update() {
-        if (Input.GetMouseButton(0)) {
+
+        if (Input.GetMouseButtonUp(0)) {
+            // Alter grab flag
+            grabbed = false;
+        }
+
+        else if (grabbed) {
+            MoveObject();
+        }
+
+        // If left mouse is clicked
+        else if (Input.GetMouseButtonDown(0)) {
+            // Get mouse position in 3D coordinates
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // Convert 3D mouse position to 2D
             Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            // Find all collider objects that hit on the mouse position
             RaycastHit2D[] hits = Physics2D.RaycastAll(mousePos2D, Vector2.zero);
 
-            if (grabbed) {
-                grabbed = false;
-            }
-            else if (hits.Length == 0) {
+            // If no objects were detected on mouse position
+            if (hits.Length == 0) {
                 Debug.Log("No collider was hit");
             }
+            // If objects were detected
             else if (hits.Length > 0) {
                 float minYCoord = 100.0f;
+                // Find the closest bottle by comparing objects' y-coord
                 foreach (RaycastHit2D hit in hits) {
-                    // Find the closest bottle
                     GameObject currObj = hit.collider.gameObject;
                     float yCoord = currObj.transform.position.y;
 
+                    // Keep track of lowest y-coordinate object
                     if (minYCoord > yCoord) {
                         minYCoord = yCoord;
                         grabbedObj = currObj;
@@ -43,37 +57,24 @@ public class ShelfMove : MonoBehaviour
                 }
                 grabbed = true;
             }
-            
-        }
-
-        if (grabbed) {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //float newObjXPos = GetNearestXCoord(mousePos.x);
-            //float newObjYPos = GetNearestYCoord(mousePos.y);
-            float newObjXPos = mousePos.x;
-            float newObjYPos = mousePos.y;
-            CheckShelfBoundary(ref newObjXPos, ref newObjYPos);
-            Vector2 newObjPos = new Vector2(newObjXPos, newObjYPos);
-            grabbedObj.transform.position = newObjPos;
         }
     }
 
     void CheckShelfBoundary(ref float newXPos, ref float newYPos) {
+        // Set y-coordinate boundaries
         newYPos = Math.Max(newYPos, shelfMinYCoord);
         newYPos = Math.Min(newYPos, shelfMaxYCoord);
 
+        // Set x-coordinate boundaries
         newXPos = Math.Max(newXPos, shelfMinXCoord);
         newXPos = Math.Min(newXPos, shelfMaxXCoord);
-
     }
 
-
-    float GetNearestXCoord(float xPos) {
-        int nearestXCoord = (int)Math.Round(xPos, 0);
-        return nearestXCoord;
-    }
-    float GetNearestYCoord(float yPos) {
-        int nearestYCoord = (int)Math.Round(yPos, 0);
-        return nearestYCoord;
+    void MoveObject() {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float newObjXPos = mousePos.x;
+        float newObjYPos = mousePos.y;
+        CheckShelfBoundary(ref newObjXPos, ref newObjYPos);
+        grabbedObj.transform.position = new Vector2(newObjXPos, newObjYPos);;        
     }
 }
